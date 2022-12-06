02c99863da897602fa882d3178833ea125f23188
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2022-12-06 14:47:52 +0000

    created buttons

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const userChoiceDisplay = document.createElement('h1')
-const computerChoiceDisplay = document.createElement('h1')
-const resultDisplay = document.createElement('h1')
-const gameGrid = document.getElementById('game')
-gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
+const userChoiceDisplay = document.createElement("h1");
+const computerChoiceDisplay = document.createElement("h1");
+const resultDisplay = document.createElement("h1");
+const gameGrid = document.getElementById("game");
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
+
+const choices = ["rock", "paper", "scissors", "spock", "lizard"];
+
+const handleClick = () => {
+  console.log("clicked");
+};
+
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement("button");
+  button.id = choices[i];
+  button.innerHTML = choices[i];
+  button.addEventListener("click", handleClick);
+  gameGrid.appendChild(button);
+}

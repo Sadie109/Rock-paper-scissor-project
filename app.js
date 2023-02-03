e186b4213e67fc778fd2e22811ea2ea7535676c8
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2023-02-03 14:58:20 +0000

    added main and game functions

--- app.js
+++ app.js
@@ -4,9 +4,37 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
-const rock_div = document.getElementById(".rock");
-const paper_div = document.getElementById(".paper");
-const scissors_div = document.getElementById(".scissors");
-const spock_div = document.getElementById(".spock");
-const lizard_div = document.getElementById(".lizard");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const spock_div = document.getElementById("spock");
+const lizard_div = document.getElementById("lizard");
 
+function game(userChoice) {
+    console.log("poop" + userChoice);
+}
+
+function main() {
+    
+rock_div.addEventListener('click', function() {
+   game("rock");
+})
+
+paper_div.addEventListener('click', function() {
+    game("paper");
+})
+
+scissors_div.addEventListener('click', function() {
+    game("scissors");
+})
+
+spock_div.addEventListener('click', function() {
+    game("spock");
+})
+
+lizard_div.addEventListener('click', function() {
+    game("lizard");
+})
+}
+
+main();
